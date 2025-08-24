2efdf25caca3b1d5290cfc9d24746dccc7397610
anujkamdar <anujkamdar2006@gmail.com>
2025-08-24 22:37:17 +0530

    added utils

---
+++ Backend/src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import CookieParser from "cookie-parser";
+import cors from "cors";
+import { ApiError } from "./utils/ApiError.js";
+
+const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(CookieParser());
+
+
+app.use((err, req, res, next) => {
+  let customError = err;
+
+  // If it's not an ApiError, convert it
+  if (!(err instanceof ApiError)) {
+    customError = new ApiError(
+      err.statusCode || 500,
+      err.message || "Internal Server Error",
+      [], // Optional: collect validation or system errors
+      process.env.NODE_ENV === "development" ? err.stack : ""
+    );
+  }
+
+  res.status(customError.statusCode).json({
+    success: customError.success,
+    message: customError.message,
+    errors: customError.errors,
+    stack: process.env.NODE_ENV === "development" ? customError.stack : undefined,
+  });
+});
+
+export { app };
